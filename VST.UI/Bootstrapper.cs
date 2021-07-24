6600b0957b4cc282019f3827004af807d9c94c1e
Jakub <jakub.jachow@gmail.com>
2021-07-24 23:55:13 +0200

    Bootstrapper

---
+++ VST.UI/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using VST.UI.ViewModels;
+
+namespace VST.UI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        #endregion
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
