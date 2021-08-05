478e9b6fcdecc2df37aa4deb0ba7d2e1417b0028
Jakub <jakub.jachow@gmail.com>
2021-08-06 01:26:11 +0200

    Media player

--- VST.UI/ViewModels/MainViewModel.cs
+++ VST.UI/ViewModels/MainViewModel.cs
@@ -1,8 +1,54 @@
 ﻿using Caliburn.Micro;
+using Microsoft.Win32;
+using System;
+using System.Windows.Media;
 
 namespace VST.UI.ViewModels
 {
     public class MainViewModel : PropertyChangedBase
     {
+        private readonly MediaPlayer mediaElement;
+
+        public MainViewModel()
+        {
+            mediaElement = new MediaPlayer();
+        }
+
+        public void SelectFileClick()
+        {
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                InitialDirectory = Environment.SpecialFolder.MyMusic.ToString(),
+
+                Filter = "Media files (*.wav)|*.wav|All Files (*.*)|*.*",
+
+                RestoreDirectory = true
+            };
+
+
+
+            if (dlg.ShowDialog() == true)
+            {
+                string selectedFileName = dlg.FileName;
+
+                mediaElement.Open(new Uri(selectedFileName));
+            }
+        }
+
+        public void PlayClick()
+        {
+            if (mediaElement != null)
+            {
+                mediaElement.Play();
+            }
+        }
+
+        public void PauseClick()
+        {
+            if (mediaElement != null && mediaElement.CanPause)
+            {
+                mediaElement.Pause();
+            }
+        }
     }
 }
