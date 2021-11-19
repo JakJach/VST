e07fa3e2d27086d72e87eeb9146c8f5b15bb2e10
Jakub <jakub.jachow@gmail.com>
2021-11-19 01:47:22 +0100

    ML - updated environment

--- VST.MachineLearning/classifier.py
+++ VST.MachineLearning/classifier.py
@@ -1,25 +1,51 @@
-import pandas as pd
+import tensorflow as tf
+import numpy as np
+import glob
 import os
 import librosa
-import librosa.display
-import numpy as np
-import IPython.display as ipd
+import pandas as pd
+import pydot
+import shutil
+import random
+
+from PIL import Image
+from IPython.display import SVG
+from pydub import AudioSegment
+
+#SCIPY
+import scipy
+from scipy import misc
+
+#MATPLOTLIB
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
-import FMA.utils as utils
+#KERAS
+from keras import layers
+from keras.layers import (Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, 
+                          Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D)
+from keras.models import Model, load_model
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import layer_utils, plot_model
+from keras.utils.vis_utils import model_to_dot
+from keras.optimizers import Adam
+from keras.initializers import glorot_uniform
 
-os.environ['KAGGLE_CONFIG_DIR'] = "/content"
+#Dataset utils
+import FMA.utils
 
-# Directory where mp3 are stored.
+
+#Directory where mp3 are stored
 AUDIO_DIR = 'D:\\MGR\\fma\\fma_small'
 METADATA_DIR = 'D:\\MGR\\fma\\fma_metadata'
 
 #Import dataset metadata
-tracks = utils.load(METADATA_DIR + '\\tracks.csv')
-features = utils.load(METADATA_DIR + '\\features.csv')
-echonest = utils.load(METADATA_DIR + '\\echonest.csv')
+tracks = FMA.utils.load(METADATA_DIR + '\\tracks.csv')
+features = FMA.utils.load(METADATA_DIR + '\\features.csv')
+echonest = FMA.utils.load(METADATA_DIR + '\\echonest.csv')
 
-file_name = utils.get_audio_path(AUDIO_DIR, 2)
+file_name = FMA.utils.get_audio_path(AUDIO_DIR, 2)
 
 print(file_name)
 
